6fcfe120f6d7a0a96ab0d8c642e6d0f72da9fdc7
Moeakug <moe.aku.g@gmail.com>
2022-05-30 23:23:51 +0200

    crash animation

--- scripts/main.js
+++ scripts/main.js
@@ -588,6 +588,10 @@ class Game {
   }
 
   _gameOver() {
+
+    setTimeout(() => {
+      this.Crash.visible = true;
+    }, 10);
     
     this.running = false;
     this.divGameOverScore.innerText = this.score;
@@ -596,7 +600,8 @@ class Game {
     setTimeout(() => {
       this.divGameOverPanel.style.display = "grid";
       this._reset(true);
-    }, 1000);
+      this.Crash.visible = false;
+    }, 3000);
   }
 
   _createPlayerCar(scene) {
@@ -669,6 +674,7 @@ class Game {
       camera.rotateX((-20 * Math.PI) / 180);
       camera.position.set(0, 1.5, 3);
     } else {
+      this.Crash.visible = false;
       this.objectsParent.traverse((item) => {
         if (item.name == "obs") {
           //console.log("kid");
@@ -699,8 +705,12 @@ class Game {
 
     //camera.rotateX((-75 * Math.PI) / 180); //top view .. needs work
     //camera.position.set(0, 8,2);
+
+
+
   }
 
+  
   _spawnTrees() {
     /* const obj = new THREE.Mesh(this.OBSTACLE_PREFAB, this.TREE_MATERIAL);
     obj.scale.set(0.25, 1, 0.25);
@@ -867,8 +877,23 @@ class Game {
     var road = new THREE.Mesh(geo, mat);
     road.position.set(0, -0.01, -10);
     road.rotation.set(-Math.PI / 2, Math.PI / 2000, Math.PI);
-    
 
+
+    var geo = new THREE.PlaneGeometry(32, 32, 1);
+    var crash = new THREE.MeshBasicMaterial();
+    var texture = new THREE.TextureLoader().load("resources/crash.png");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.flipY =true;
+    texture.repeat.set( 5, 5 );
+    crash.map = texture;
+    this.Crash = new THREE.Mesh(geo, crash);
+    this.Crash.position.set(0, 0.01,0 );
+    this.Crash.rotation.set(-Math.PI / 2, Math.PI / 2000, Math.PI);
+    this.Crash.scale.set(-1,-1,1);
+    this.Crash.visible = false;
+    
+    this.roadLineParent.add(this.Crash);
     this.roadLineParent.add(road);
     this.roadLineParent.add(laneLine_1);
     this.roadLineParent.add(laneLine_2);
